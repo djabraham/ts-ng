3ef8e7468538caa571e9762285f6be17bce97710
djabraham <abrahamdanielj@gmail.com>
2016-02-08 00:04:45 -0500

    Initial Pages

--- src/server/tsd.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../typings/tsd.d.ts" />
-/// <reference path="../SharedTypes.d.ts" />
-
-// declare module express {
-//     export interface Request {
-//         rootPath: string;
-//     }
-// }
-//
